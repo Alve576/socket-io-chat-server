d55e697911477d7c75a1c9830e8882d9a395f2b0
Alve576 <shadmanzz656@gmail.com>
2022-06-11 19:57:24 +0600

    first commit

--- index.js
+++ index.js
@@ -17,18 +17,31 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
+// audio call
+  socket.emit('me',socket.id)
+
   socket.on('join_room',(data)=>{
       socket.join(data);
       console.log(`user id  :  ${socket.id} joined room : ${data}`)
 
-  })
+  });
   socket.on("send_message",(data)=>{
       console.log(data)
       socket.to(data.room).emit('receive_message',data);
 
   });
+// audio call
+
   socket.on("disconnect", () => { 
+    socket.broadcast.emit('call ended');    
     console.log("User Disconnected", socket.id);
+  });
+  socket.on('calluser',({userToCall,signalData,from,name})=>{
+    io.to(userToCall).emit('calluser',{signal:signalData,from,name})
+  });
+
+  socket.on('answercall',(data)=>{
+    io.to(data.to).emit("callaccepted",data.signal)
   });
 });
 
